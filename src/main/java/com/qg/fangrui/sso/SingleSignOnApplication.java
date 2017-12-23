fe9740b35491ef4e1c3a9f095606dbef768d8f4f
FunriLy <fr479910770@qq.com>
2017-12-23 20:24:55 +0800

    init project

---
+++ src/main/java/com/qg/fangrui/sso/SingleSignOnApplication.java
@@ -0,0 +1,18 @@
+package com.qg.fangrui.sso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动主类
+ * @author FunriLy
+ * Created by FunriLy on 2017/12/23.
+ * From small beginnings comes great things.
+ */
+@SpringBootApplication
+public class SingleSignOnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SingleSignOnApplication.class, args);
+	}
+}
